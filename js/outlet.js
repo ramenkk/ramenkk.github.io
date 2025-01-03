9927ee75f01bb17cf7622efc477a9192ad0690d6
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2025-01-03 13:35:14 +0700

    edit fetch API

--- js/outlet.js
+++ js/outlet.js
@@ -10,12 +10,15 @@ document.getElementById('outletForm').addEventListener('submit', function (e) {
     } else {
         errorMessage.style.display = 'none';
 
-        // Simulasi validasi backend
-        fetch(`https://asia-southeast2-menurestoran-443909.cloudfunctions.net/menurestoran/data/outlet?kode_outlet=RAMENBDG01`)
-            .then(response => {
-                if (response.ok) {
-                    // Redirect ke halaman menu
-                    window.location.href = `/pages/index.html?kode_outlet=${kodeOutlet}`;
+        
+        fetch(`https://asia-southeast2-menurestoran-443909.cloudfunctions.net/menurestoran/data/outlet`)
+            .then(response => response.json()) 
+            .then(data => {
+               
+                const outlet = data.find(outlet => outlet.kode_outlet === kodeOutlet);
+                if (outlet) {
+                  
+                    window.location.href = `../pages/index_pages.html?kode_outlet=${kodeOutlet}`;
                 } else {
                     errorMessage.textContent = 'Kode outlet tidak valid!';
                     errorMessage.style.display = 'block';
