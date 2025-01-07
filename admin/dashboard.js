5449d97de0cfec190dd19f53c93f9f0cc18c12de
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2025-01-07 22:25:15 +0700

    add auth dashboard

---
+++ admin/dashboard.js
@@ -0,0 +1,116 @@
+import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js";
+import {addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.9/element.js";
+
+addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+        Swal.fire({
+            icon: "warning",
+            title: "Can't Access Dashboard",
+            text: "You need to login before accessing dashboard!",
+            timer: 2000,
+            customClass: {
+                container: 'backdrop-blur-md',
+            },
+            showConfirmButton: false
+          });
+          setTimeout(() => {
+            window.location.href = '../login/login.html';
+          }, 2000);
+        return;
+    }
+
+    try {
+        const response = await fetch('https://asia-southeast2-menurestoran-443909.cloudfunctions.net/menurestoran/admin/dashboard', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        });
+
+        const data = await response.json();
+
+        if (response.status === 200) {
+            if (!localStorage.getItem('alertShown')) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    backdrop: false,
+                    customClass: {
+                        container: 'no-blur-container',
+                    },
+                    didOpen: (toast) => {
+                      toast.onmouseenter = Swal.stopTimer;
+                      toast.onmouseleave = Swal.resumeTimer;
+                    }
+                });
+                Toast.fire({
+                    icon: "success",
+                    title: "Anda telah masuk",
+                    text: data.message + " dengan ID Admin: " + data.admin_id,
+                });
+                localStorage.setItem('alertShown', 'true');
+            }
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Access Restricted",
+                text: "Cannot access dashboard!",
+                timer: 2000,
+                backdrop: true,
+                customClass: {
+                    container: 'backdrop-blur-md',
+                },
+                showConfirmButton: false
+            });
+            setTimeout(() => {
+                window.location.href = '../login/login.html';
+            }, 2000);
+        }
+    } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Access Restricted",
+            text: data.message,
+            timer: 2000,
+            backdrop: true,
+            customClass: {
+                container: 'backdrop-blur-md',
+            },
+            showConfirmButton: false
+        });
+        setTimeout(() => {
+            window.location.href = '../login/login.html';
+        }, 2000);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (localStorage.getItem('cancelToast') === 'true') {
+
+      localStorage.removeItem('cancelToast');
+  
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+  
+      Toast.fire({
+        icon: "info",
+        title: "You canceled the action."
+      });
+    }
+});
--- js/login.js
+++ js/login.js
@@ -1,51 +1,57 @@
-const form = document.querySelector('form'); // Select the form element
-const kodeOutletInput = document.getElementById('kode_outlet'); // Corrected to match the HTML id
-const errorMessage = document.getElementById('errorMessage'); // Corrected the element for error message
+import Swal from "https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js";
+import {addCSS} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.9/element.js";
 
-// Event listener for form submit
-form.addEventListener('submit', (e) => {
-  e.preventDefault(); // Prevent page reload
-  const kodeOutlet = kodeOutletInput.value.trim();
+addCSS("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
 
-  if (!kodeOutlet) {
-    showError("Kode Outlet tidak boleh kosong!");
-    return;
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Login function
+  async function login(username, password) {
+    try {
+      const response = await fetch('https://asia-southeast2-menurestoran-443909.cloudfunctions.net/menurestoran/admin/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ Username: username, Password: password })
+      });
+
+      const data = await response.json();
+
+      if (response.status === 200) {
+        localStorage.setItem('token', data.token);
+        Swal.fire({
+          icon: 'success',
+          title: 'Login Successful',
+          text: 'Login Succes direct to dashboard.',
+          timer: 2000,
+          showConfirmButton: false
+        });
+        setTimeout(() => {
+          window.location.href = '../admin/admin.html';
+        }, 2000);
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Login Failed',
+          text: 'Username atau password salah!',
+        });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      Swal.fire({
+        icon: 'warning',
+        title: 'Login Failed',
+        text: 'Terjadi kesalahan pada server, coba lagi nanti.'
+      });
+    }
   }
 
-  // Validate kode outlet with the API
-  validateKodeOutlet(kodeOutlet)
-    .then(outletID => {
-      // After successful validation, redirect to the menu page
-      window.location.href = `../admin/index.html?outlet_id=${outletID}`;
-
-    })
-    .catch(error => {
-      showError(error.message);
-    });
+  document.getElementById('loginForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = document.getElementById('usernameInput').value;
+    const password = document.getElementById('passwordInput').value;
+    login(username, password);
+  });
 });
-
-function showError(message) {
-  errorMessage.textContent = message;
-  errorMessage.style.display = 'block'; // Show the error message
-}
-
-function hideError() {
-  errorMessage.style.display = 'none'; // Hide the error message
-}
-
-function validateKodeOutlet(kodeOutlet) {
-  return fetch(`https://asia-southeast2-menurestoran-443909.cloudfunctions.net/menurestoran/data/validate?kode_outlet=${kodeOutlet}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Kode outlet tidak valid atau tidak ditemukan.");
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.status === "success") {
-        return data.outlet_id; 
-      } else {
-        throw new Error("Kode outlet tidak valid atau tidak ditemukan.");
-      }
-    });
-}
